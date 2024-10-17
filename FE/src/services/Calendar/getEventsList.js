165d74bf49c97786209e2b851ffc7238ec65ce48
Park Jong Hyuk <occultism22@naver.com>
2024-10-18 02:45:32 +0900

    Feat: 캘린더-친구들의 펀딩목록 불러오기 api 컴포넌트화

---
+++ FE/src/services/Calendar/getEventsList.js
@@ -0,0 +1,18 @@
+import axiosInstance from "../../@common/axiosInstance";
+
+const getEventsList = async(year, month) => {
+  try {
+    const response = await axiosInstance.get("/api/fundings/calendar", {
+      params: {
+        year,
+        month,
+      },
+    });
+    return response.data.data;
+  }
+  catch (err) {
+    console.error("친구들의 펀딩목록 불러오기 실패", err);
+  };
+};
+
+export default getEventsList;
