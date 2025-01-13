f2463050c303310be33f19a31a39676040cf1667
Park Jong Hyuk <occultism22@naver.com>
2025-01-13 15:41:13 +0900

    Feat: 주소 목록 api 관리방식 변경 주소 목록 호출에 react-query 적용

--- FE/src/pages/Funding/AddressListPage.jsx
+++ FE/src/pages/Funding/AddressListPage.jsx
@@ -1,28 +1,24 @@
 import AddressList from "../../components/Funding/component/AddressList";
 import { useStore } from "../../components/Store/MakeStore";
 import { useNavigate, useLocation } from "react-router-dom"; // useNavigate 사용
-import { getAddressList } from "../../services/Address/getAddressList";
-import { useEffect, useState } from "react";
+import { getAddressList } from "../../services/Address/addresses";
+import { useQuery } from "@tanstack/react-query";
 
 function AddressListPage() {
   const navigate = useNavigate();
   const setContentIndex = useStore((state) => state.setContentIndex);
-
-  const [addressList, setAddressList] = useState([]);
   const location = useLocation();
 
-  //처음 화면시작 시 주소 불러와서 list에 추가
-  useEffect(() => {
-    const token = localStorage.getItem("access-token");
-    if (token) {
-      getAddressList(token, setAddressList);
-    }
-  }, []);
-
-  //디버깅 : 주소 목록이 바뀔 때 마다 list console 출력
-  useEffect(() => {
-    console.log(addressList);
-  }, [addressList]);
+  // data: 쿼리 함수 호출로 인해 얻는 data
+  // data에 기본값으로 addressList = [] 설정했는데, 그냥 data로 사용하고 props도 data로 전달해줘도 된다
+  // isLoading: 데이터 불러오는 상태
+  const { data: addressList = [], isLoading} = useQuery({
+    queryKey: ["주소 목록"], // 해당 쿼리에 대한 key값, 나중에 mutate ( 쿼리 수정 요청할 때 ) 사용된다
+    queryFn: getAddressList, // 불러올 data에 대한 함수 ( api 요청 등등.. )
+    onError: (err) => { // 쿼리 호출 실패했을 때
+      console.error(err);
+    },
+  });
 
   const handleSelectButtonClick = () => {
     setContentIndex(3); // 컨텐츠 인덱스를 3으로 설정
@@ -31,7 +27,7 @@ function AddressListPage() {
     });
   };
 
-  if (!addressList) return <div>주소 목록 로딩 중</div>;
+  if (isLoading) return <div>주소 목록 로딩 중</div>;
   return (
     <div className="sub-layer font-cusFont3 text-[14px]">
       <AddressList listData={addressList} />
